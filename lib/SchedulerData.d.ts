0dbbcfadc70a873d60d19a7fba3db4145aaeb089
Yunus Demir <github@yunusdemir.nl>
2021-12-10 18:17:41 +0100

    Next try

--- lib/SchedulerData.d.ts
+++ lib/SchedulerData.d.ts
@@ -1,5 +1,5 @@
 export default class SchedulerData {
-    constructor(date?: string, viewType?: number, showAgenda?: boolean, isEventPerspective?: boolean, newConfig?: any, newBehaviors?: any, localeMoment?: any);
+    constructor(date?: string, viewType?: number, showAgenda?: boolean, isEventPerspective?: boolean, newConfig?: any, newBehaviors?: any, localeMoment?: any, documentWidth?: number);
     resources: any[];
     events: any[];
     eventGroups: any[];
